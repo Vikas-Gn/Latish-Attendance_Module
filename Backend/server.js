d4e106640e88b5fd38bdb755140c1086824a11b0
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-23 14:43:58 +0530

    updated-code

--- Backend/server.js
+++ Backend/server.js
@@ -192,5 +192,5 @@ app.delete('/api/records', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://3.110.120.41:${port}`);
+    console.log(`Server running at http://13.233.229.154:${port}`);
 });
